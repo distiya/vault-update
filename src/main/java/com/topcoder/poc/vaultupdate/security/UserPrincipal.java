b5ca29a88aa11a60ffa12848bf1be45493a5078a
sasitha <sasithaniranjana@gmail.com>
2021-02-19 15:58:01 +0800

    Added default username and password

--- src/main/java/com/topcoder/poc/vaultupdate/security/UserPrincipal.java
+++ src/main/java/com/topcoder/poc/vaultupdate/security/UserPrincipal.java
@@ -18,7 +18,7 @@ import org.springframework.cloud.endpoint.RefreshEndpoint;
 @ConfigurationProperties
 public class UserPrincipal {
 
-    private String securityUserName;
-    private String securityPassword;
+    private String securityUserName = "vaultUser";
+    private String securityPassword = "vaultPassword";
 
 }
